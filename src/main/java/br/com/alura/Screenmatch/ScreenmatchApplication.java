da94ba4e38c95d06603fbef9511ea0061e2f61aa
Gabriel monteiro <monteiro.silva.gabriel2005@gmail.com>
2026-01-12 17:10:31 -0300

    Inicio do projeto Spring

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.Screenmatch;
 
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.service.ConsumoAPI;
+import br.com.alura.Screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +17,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primerio projeto Spring sem Web");
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=df68f865");
+		//System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
