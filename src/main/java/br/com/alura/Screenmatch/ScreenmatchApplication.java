dfcc85b0e35079ef988f5a4922b067f7bd30edb6
Gabriel monteiro <monteiro.silva.gabriel2005@gmail.com>
2026-01-13 21:05:47 -0300

    Implementação classe principal

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.Screenmatch;
 
-import br.com.alura.Screenmatch.model.DadosSerie;
-import br.com.alura.Screenmatch.service.ConsumoAPI;
-import br.com.alura.Screenmatch.service.ConverteDados;
+import br.com.alura.Screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,13 +15,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=df68f865");
-		//System.out.println(json);
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
