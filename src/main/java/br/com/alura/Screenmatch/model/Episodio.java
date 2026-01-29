651deb14f23c3a3dd6616a5fab098113bdbf2809
Gabriel monteiro <monteiro.silva.gabriel2005@gmail.com>
2026-01-28 21:42:26 -0300

    Implementação de filtros e tratamento de dados

--- src/main/java/br/com/alura/Screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/Screenmatch/model/Episodio.java
@@ -10,20 +10,20 @@ public class Episodio {
     private Double avaliacao;
     private LocalDate dataLancamento;
 
-    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio){
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
         this.titulo = dadosEpisodio.titulo();
         this.numeroEpisodio = dadosEpisodio.numero();
 
-        try{
+        try {
             this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
-        } catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             this.avaliacao = 0.0;
         }
 
         try {
             this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
-        } catch (DateTimeParseException ex){
+        } catch (DateTimeParseException ex) {
             this.dataLancamento = null;
         }
     }
@@ -32,30 +32,48 @@ public class Episodio {
         return temporada;
     }
 
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
     public String getTitulo() {
         return titulo;
     }
 
-    public Integer getNumero() {
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
         return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
     }
 
     public Double getAvaliacao() {
         return avaliacao;
     }
 
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
     public LocalDate getDataLancamento() {
         return dataLancamento;
     }
 
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
     @Override
     public String toString() {
-        return  "temporada=" + temporada +
+        return "temporada=" + temporada +
                 ", titulo='" + titulo + '\'' +
                 ", numeroEpisodio=" + numeroEpisodio +
                 ", avaliacao=" + avaliacao +
-                ", dataLancamento=" + dataLancamento;
+                ", dataLancamento=" + dataLancamento ;
     }
 }
-
-
